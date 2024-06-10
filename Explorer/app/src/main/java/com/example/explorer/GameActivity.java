8af56931f86e6caeef5d112c2aaa57a062827354
dressmer <129370965+dressmer@users.noreply.github.com>
2024-06-10 14:10:14 +0200

    Upload de mon magnifique travail

---
+++ Explorer/app/src/main/java/com/example/explorer/GameActivity.java
@@ -0,0 +1,150 @@
+package com.example.explorer;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.InputStream;
+import java.util.Scanner;
+
+public class GameActivity extends AppCompatActivity {
+    private JSONObject data;
+    private int location = -1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+
+
+        Intent intent = getIntent();
+        Bundle extras = intent.getExtras();
+        String filename = null;
+        if (extras != null) {
+            filename = extras.getString("filename");
+        }
+
+        Resources res = this.getResources();
+        int sourcefile = res.getIdentifier(filename, "raw", this.getPackageName());
+        String worldtitle = "";
+        int startLocation = 0;
+        InputStream inputStream = getResources().openRawResource(sourcefile);
+        Scanner scanner = new Scanner(inputStream);
+        String jsonString = scanner.useDelimiter("\\A").next();
+        try {
+            data = new JSONObject(jsonString);
+            worldtitle = data.getString("title");
+            startLocation = data.getInt("start");
+
+            /*
+            JSONObject location = data.getJSONArray("places").getJSONObject(startLocation);
+            int imageResourceId = this.getResources().getIdentifier(location.getString("image"), "drawable", this.getPackageName());
+            ImageView image = findViewById(R.id.locationImage);
+            image.setImageResource(imageResourceId);
+
+            TextView name = findViewById(R.id.locationName);
+            name.setText(location.getString("name"));
+
+            TextView desc = findViewById(R.id.locationDesc);
+            desc.setText(location.getString("desc"));
+
+             */
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        String newtitle = String.format(getString(R.string.app_title_name), getString(R.string.app_name), worldtitle);
+        setTitle(newtitle);
+
+        Log.d("APP", "CREATE");
+        if (filename != null) {
+            Log.d("APP", "filename: " + filename);
+        }
+
+        setLocation(0);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        Log.d("APP","RESTORE");
+        this.setLocation(savedInstanceState.getInt("location"));
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        Log.d("APP","SAVE");
+        outState.putInt("location", location);
+    }
+
+    protected void setLocation(int newLoc) {
+        location = newLoc;
+        try {
+            JSONObject locationObject = data.getJSONArray("places").getJSONObject(location);
+            TextView locationTitleTextView = findViewById(R.id.locationName);
+            locationTitleTextView.setText(locationObject.getString("name"));
+            TextView locationDescTextView = findViewById(R.id.locationDesc);
+            locationDescTextView.setText(locationObject.getString("desc"));
+            ImageView locationImage = findViewById(R.id.locationImage);
+            locationImage.setVisibility(View.GONE);
+            locationImage.invalidate();
+
+
+
+            if (locationObject.has("image")) {
+                locationImage.setVisibility(View.VISIBLE);
+                int resourceId = getResources().getIdentifier(locationObject.getString("image"), "drawable", getPackageName());
+                locationImage.setImageResource(resourceId);
+            } else {
+                locationImage.setVisibility(View.GONE);
+            }
+
+            LinearLayout buttonsContainer = findViewById(R.id.buttons_container);
+            JSONArray actions = locationObject.getJSONArray("actions");
+            buttonsContainer.removeAllViews();
+            for (int i = 0; i < actions.length(); i++) {
+                JSONObject action = actions.getJSONObject(i);
+                String actionName = action.getString("action_name");
+                Button button = new Button(this);
+                button.setText(actionName);
+                int next = action.getInt("next");
+                button.setOnClickListener(view -> {
+                    setLocation(next);
+                });
+                buttonsContainer.addView(button);
+            }
+
+            if (locationObject.getBoolean("final")) {
+                Button button = new Button(this);
+                button.setText(getResources().getString(R.string.won_game));
+                button.setOnClickListener(view -> {
+                    Intent intent = new Intent();
+                    intent.putExtra("win", true);
+                    setResult(Activity.RESULT_OK, intent);
+                    finish();
+                });
+                buttonsContainer.addView(button);
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ Explorer/app/src/main/java/com/example/explorer/MainActivity.java
@@ -0,0 +1,86 @@
+package com.example.explorer;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private int playCount = 0;
+    private int winCount = 0;
+
+    private TextView playCountText;
+    private TextView wonCountText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        ActivityResultLauncher<Intent> mStartForResult = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(ActivityResult result) {
+                        if (result.getResultCode() == Activity.RESULT_OK) {
+                            Intent intent = result.getData();
+                            // Handle the Intent
+                            boolean win = false;
+                            if (intent != null) {
+                                win = intent.getBooleanExtra("win", false);
+                                if (win) {
+                                    winCount++;
+                                }
+                                playCount++;
+                                updateCounters();
+                            }
+                        }
+                    }
+                }
+        );
+        Button mButtonStartGame = findViewById(R.id.button);
+        mButtonStartGame.setOnClickListener(view -> startGameActivity(mStartForResult));
+
+        playCountText = findViewById(R.id.countGamesPlayed);
+        wonCountText = findViewById(R.id.countGamesWon);
+        updateCounters();
+    }
+
+    private void startGameActivity(ActivityResultLauncher<Intent> mStartForResult) {
+        Intent intent;
+        intent = new Intent(this, GameActivity.class);
+        intent.putExtra("filename", "test");
+        //startActivity(intent);
+
+
+        mStartForResult.launch(intent);
+
+    }
+
+    private void updateCounters() {
+        playCountText.setText(Integer.toString(playCount));
+        wonCountText.setText(Integer.toString(winCount));
+    }
+
+
+
+}
